fbf5e33f402c152c630e1e7b3d5299f754bd0485
laurenlatte <lauren.nagy97@gmail.com>
2025-03-02 15:05:27 -0800

    Initial

---
+++ src/Homepage/homepage.js
@@ -0,0 +1,71 @@
+
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Overview</h1>
+      <p>This website details my life in regards to my time in corporate espionage and the psyops that ensued following my return to the United States.</p>
+      <br />
+      <p>In the last 2010s and early 2020s, I was working for two corporations in Helsinki which I will describe here.
+      The first one is Verto Analytics; a company that monitored US citizens via spyware we payed people to install to track their digital lives for marketing research.
+      The second company is Tuxera, who put me to work on a project known as Vulkan TSMB.
+      </p>
+      <a href="https://www.spiegel.de/international/world/the-vulkan-files-a-look-inside-putin-s-secret-plans-for-cyber-warfare-a-4324e76f-cb20-4312-96c8-1101c5655236">
+        Vulkan Files Reference
+      </a>
+      <br />
+      <h1>Details</h1>
+      <details>
+        <summary>Before Finland</summary>
+        <p>Right before I moved to Finland, I was taken into a ward in California. I had no psychosis or suicial ideation, I just went to the hospital so they could help me quit the xanax I was prescribed
+        because I wanted a clearer head. While in the ward, I spent a lot of time coding on the computer they had because I was passionate about code. I also tried playing my European citizenship card to get out,
+        seeing if I could be extradited from the ward. A silly notion, but I was a teenager at the time. Once it finally became time to leave, I was given an option of rehabs to go to after. One of them said Venice,
+        and as I thought it was in Europe I chose it. It turned out to be Venice Beach Los Angeles. When I arrived, the intake lady told me that they took a lot of talented people through there, including a girl with
+        photographic memory. While I was in there, they taught me a lot about the workings of the human mind, as well as emotions. Immediately after being discharged, I took some craigslist gigs and bought a one way
+        ticket to Finland to be with my long distance girlfriend at the time.
+        </p>
+      </details>
+      <details>
+        <summary>Verto Analytics</summary>
+        <p>I began work at Verto Analytics in 2018. I was originally part of the Community Management team, managing social media for the company as well as responding to emails from the folks who installed our spyware app.
+        They quickly noticed I was capable of much more and promoted me to a junior sysadmin position. I worked my way up from there and soon I had access to the whole company, from finances to users. Around this time I installed
+        Firestorm Second Life viewer and Runelite on my work laptop, creating a way for third parties to access my laptop. Im not sure if anyone ever did, but they didnt need to.
+        Over the years, the company had been firing a lot of people and each time they did, I was given their work. It was my first office job so I was a little yes man and took on as much as they could give me. Soon, I became
+        the only person in the company with knowledge of several business critical processes. Then, out of nowhere, my mental health severely deteriorated and I was out of commission for months. The company was then sold
+        to a Californian data company known as Disqo. I believe my absence coupled with my knowledge of business critical processes played a role. This one may be a bit of a stretch but I do feel the overall takeaway here is I was saboteur, unbeknownst to me at the time.
+        </p>
+      </details>
+      <details>
+        <summary>Tuxera</summary>
+        <p>After Verto Analytics was sold, I was recruited for Tuxera. They immediately put me on a project known as Vulkan TSMB. It was remote filesystem access software.
+        I worked on their build and test environments as a devops engineer. This led to me having the source code for the project on my laptop. Nothing of note really happened during my time at the company,
+        but once I left with the laptop and source code things got weird.
+        </p>
+      </details>
+      <details>
+        <summary>The War</summary>
+        <p>At 24 years old, never in my life have I had a mania or psychosis. I was living in Helsinki, on sick leave from Tuxera due to depression. I was having some strange symptoms of psychosis in the days leading up to the war. Then, right when the war begun, I woke up as a sleeper. I suddenly believed I was an agent, and I knew how to carry myself
+        as intelligence in a warzone. This was triggered fully when I saw an image of Russian tanks on the television. My phone suddenly had a software update the day war broke out and it showed an android logo with a hat that had the hammer and sickle on it. I began texting
+        strange things to my parents so my father from California came and visited me in Helsinki a few days before the war. When the war broke out, he took me back to California. I took with me both the phone that had the hammer and sickle logo, and the Tuxera laptop with Vulkan data on it.
+        </p>
+      </details>
+      <details>
+        <summary>Homecoming</summary>
+        <p>My parents immediately threw me in the ward when I returned to California, as I was acting as an activated sleeper and they had no idea what was going on. When I was in the ward, a lady came up to me and said "as long as I'm here you're safe". Once she left, I woke up screaming
+        as someone had their fingers on my neck in the middle of the night. I was asked by another woman if I would like to work for the United States. I responded with "what has this country ever done for me". Someone else came and tried teaching me encryption. They wrote out some letters and colored them then asked me to solve a scrambled word. I didn't take to it. Eventually I was released.
+        </p>
+      </details>
+      <details>
+        <summary>A year later</summary>
+        <p>
+          The Vulkan Files are finally leaked. I found a boyfriend in the Netherlands and I was planning on leaving the country to move in with him. I told him about my experiences and he believed that I was an agent of espionage. Suddenly, right before I was set to leave the country, I got a psychosis.
+          In this psychosis, I started to feel as if something else was controlling what I was saying and doing. I went to the ward and it felt like something was speaking through me. I told everybody I was a pedophile (which Im not) and the police came and confiscated all of my electronics, including the Vulkan laptop.
+          I believe that the reason I was made to tell people that I was a pedo was so that the government could confiscate my electronics. After my stay in the ward, I started going to this therapy place called Connections. A lady there played a video for me about Brain Machine Interfaces, and how they are used in the military.
+          This video, coupled with the fact that I was doing and saying things against my will, made me start to believe that I had something in my head. I eventually did end up moving to the Netherlands but only for a short time, as for some reason while I was there I felt horrible and anhedonic. I lived there for roughly a month and moved back.
+          The police ended up holding onto my devices for a year, and there was nothing on my devices hinting that I was a pedo so I was left alone. I have reason to believe they never got to the Vulkan data as they asked me for the encryption key which I was not able to give them. The laptop was dual booted so if they had switched the boot order to Windows and turned secure boot on they could have accessed the Vulkan data, but Im not sure if they did.
+          I still have the laptop.
+        </p>
+      </details>
+    </div>
+  )
+}
